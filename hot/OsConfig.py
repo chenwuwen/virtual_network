1b65ee9690047f564a9da6c297297f6f22b1107b
chenwuwen <git.123>
2020-08-14 16:02:30 +0800

    更改部分信息

--- hot/OsConfig.py
+++ hot/OsConfig.py
@@ -1,13 +1,13 @@
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 '''
 开启热点配置：
     windows下开启热点,需要将目前连接网络的适配器(是本地网络入口的适配器，不是刚刚新建的)右键--->共享---->勾选允许其他网络用户链接，
 '''
 OS = {
-    "WINDOWS":{
-        "CREATE_HOST":"netsh wlan set hostednetwork mode=allow ssid=WIFI_NAME key=WIFI_PASSWORD",
-        "START_HOT":"netsh wlan start hostednetwork",
-        "STOP_HOT":"netsh wlan stop hostednetwork"
+    "WINDOWS": {
+        "CREATE_HOST": "netsh wlan set hostednetwork mode=allow ssid=WIFI_NAME key=WIFI_PASSWORD",
+        "START_HOT": "netsh wlan start hostednetwork",
+        "STOP_HOT": "netsh wlan stop hostednetwork"
     },
     "LINUX": {
         "CREATE_HOST": "netsh wlan set hostednetwork mode=allow ssid=WIFI_NAME key=WIFI_PASSWORD",
